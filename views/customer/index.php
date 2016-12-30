64f9b1bee9b97c75b683b4c238280b1e64028e34
kientn16 <ngockien16@gmail.com>
2016-12-30 16:27:48 +0700

    fix user

--- views/customer/index.php
+++ views/customer/index.php
@@ -25,59 +25,59 @@ $this->params['breadcrumbs'][] = $this->title;
 	<?= $this->render('/_alert', [
 		'module' => Yii::$app->getModule('user'),
 	]) ?>
-<!--	--><?php //$form = ActiveForm::begin([
-//		'layout'  => 'horizontal',
-//		'options' => [
-//			'enctype' => 'multipart/form-data',
-//		],
-//	]); ?>
-<!--	--><?//= $form->field($file, 'excel', [
-//	])->widget(FileInput::className(), [
-//		'pluginOptions' => [
-//			'allowedFileExtensions' => [
-//				'xlsx',
-//			],
-//			'showUpload'            => false,
-//		],
-//	]) ?>
-<!--	<!--	<button style="float: right;clear: right">-->--><?php ////echo  ?><!--<!--</button>-->-->
-<!--	--><?//= Html::submitButton('Import Excel', [
-//		'class'    => 'btn green uppercase col-sm-offset-3',
-//		'tabindex' => '3',
-//		//		'style'    => 'float: right;clear: right',
-//	]) ?>
-<!--	--><?php //ActiveForm::end(); ?>
-<!--	--><?//= GridView::widget([
-//		'dataProvider' => $dataProvider,
-//		'export'       => false,
-//		//		'pjax'         => true,
-//		'columns'      => [
-//			[
-//				'class'  => 'kartik\grid\SerialColumn',
-//				'header' => 'STT',
-//			],
-//			'created_date',
+	<?php $form = ActiveForm::begin([
+		'layout'  => 'horizontal',
+		'options' => [
+			'enctype' => 'multipart/form-data',
+		],
+	]); ?>
+	<?= $form->field($file, 'excel', [
+	])->widget(FileInput::className(), [
+		'pluginOptions' => [
+			'allowedFileExtensions' => [
+				'xlsx',
+			],
+			'showUpload'            => false,
+		],
+	]) ?>
+	<!--	<button style="float: right;clear: right">--><?php //echo  ?><!--</button>-->
+	<?= Html::submitButton('Import Excel', [
+		'class'    => 'btn green uppercase col-sm-offset-3',
+		'tabindex' => '3',
+		//		'style'    => 'float: right;clear: right',
+	]) ?>
+	<?php ActiveForm::end(); ?>
+	<?= GridView::widget([
+		'dataProvider' => $dataProvider,
+		'export'       => false,
+		//		'pjax'         => true,
+		'columns'      => [
+			[
+				'class'  => 'kartik\grid\SerialColumn',
+				'header' => 'STT',
+			],
+			'created_date',
 //			[
 //				'class'           => 'kartik\grid\EditableColumn',
 //				'attribute'       => 'name',
 //				'editableOptions' => [
 //					'inputType' => \kartik\editable\Editable::INPUT_TEXT,
 //					'asPopover' => false
-//					//					'options'=>[
-//					//						'pluginOptions'=>['min'=>0, 'max'=>5000]
-//					//					]
+					//					'options'=>[
+					//						'pluginOptions'=>['min'=>0, 'max'=>5000]
+					//					]
 //				],
 //			],
-//			'address',
-//			[
-//				'attribute' => 'birthday',
-//				'value'     => function (Customer $data) {
-//					return $data->birthday != null ? Yii::$app->formatter->asDate($data->birthday, 'd-M-Y') : '';
-//				},
-//			],
-//			'email:email',
-//			'phone',
-//			'id_number',
+			'address',
+			[
+				'attribute' => 'birthday',
+				'value'     => function (Customer $data) {
+					return $data->birthday != null ? Yii::$app->formatter->asDate($data->birthday, 'd-M-Y') : '';
+				},
+			],
+			'email:email',
+			'phone',
+			'id_number',
 //			[
 //				'class'           => 'kartik\grid\EditableColumn',
 //				'attribute'       => 'city_id',
@@ -86,71 +86,69 @@ $this->params['breadcrumbs'][] = $this->title;
 //					'data'      => ArrayHelper::map(\app\models\City::find()->andWhere([
 //						'status' => 1,
 //					])->all(), 'name', 'name'),
-//					'asPopover' => false
-//
-//					//					'options'     => [
-//					//						'type'=>\kartik\datecontrol\DateControl::FORMAT_DATE,
-//					//						'displayFormat'=>'dd.MM.yyyy',
-//					//						'saveFormat'=>'php:Y-m-d',
-//					//
-//					//						'options'   => [
-//					//							'pluginOptions' => [
-//					//								'autoclose' => true,
-//					//							],
-//					//						],
-//					//					],
+					//					'options'     => [
+					//												'type'=>\kartik\datecontrol\DateControl::FORMAT_DATE,
+					//												'displayFormat'=>'dd.MM.yyyy',
+					//						'saveFormat'=>'php:Y-m-d',
+					//
+					//						'options'   => [
+					//							'pluginOptions' => [
+					//								'autoclose' => true,
+					//							],
+					//						],
+					//					],
 //				],
 //			],
-//			[
-//				'attribute' => 'user_id',
-//				'value'     => function (Customer $data) {
-//					return $data->user->username;
-//				},
-//			],
-//			// 'point',
-//			[
-//				'attribute' => 'parent_id',
-//				'value'     => function (Customer $data) {
-//					return $data->parent->username;
-//				},
-//			],
-//			'source',
-//			'product',
-//			// 'is_move',
-//			'link_fb',
-//			// 'sale',
-//			'note:ntext',
-//			[
-//				'attribute' => 'is_call',
-//				'value'     => function (Customer $data) {
-//					return $data::IS_CALL[$data->is_call];
-//				},
-//			],
-//			[
-//				'attribute' => 'count',
-//				'value'     => function (Customer $data) {
-//					return $data->getProductCount();
-//				},
-//			],
-//			'call_by',
-//			'call_at',
-//			[
-//				'class'          => 'kartik\grid\ActionColumn',
-//				'header'         => 'Hành động',
-//				'visibleButtons' => [
-//					'view'   => RoleChecker::isAuth(CustomerItemController::className(), 'detail'),
-//					'update' => RoleChecker::isAuth(CustomerController::className(), 'update'),
-//					'delete' => RoleChecker::isAuth(CustomerController::className(), 'delete'),
-//				],
-//				'buttons'        => [
-//					'view' => function ($url, $model, $key) {
-//						return Html::a('<i class="fa fa-shopping-cart" aria-hidden="true"></i>', Url::to([
-//							'customer-item/detail',
-//							'id' => $model->id,
-//						]));
-//					},
-//				],
-//			],
-//		],
-//	]); ?>
+			[
+				'attribute' => 'user_id',
+				'value'     => function (Customer $data) {
+					return $data->user->username;
+				},
+			],
+			// 'point',
+			[
+				'attribute' => 'parent_id',
+				'value'     => function (Customer $data) {
+					return $data->parent->username;
+				},
+			],
+			'source',
+			'product',
+			// 'is_move',
+			'link_fb',
+			// 'sale',
+			'note:ntext',
+			[
+				'attribute' => 'is_call',
+				'value'     => function (Customer $data) {
+					return $data::IS_CALL[$data->is_call];
+				},
+			],
+			[
+				'attribute' => 'count',
+				'value'     => function (Customer $data) {
+					return $data->getProductCount();
+				},
+			],
+			'call_by',
+			'call_at',
+			[
+				'class'          => 'kartik\grid\ActionColumn',
+				'header'         => 'Hành động',
+				'visibleButtons' => [
+					'view'   => RoleChecker::isAuth(CustomerItemController::className(), 'detail'),
+					'update' => RoleChecker::isAuth(CustomerController::className(), 'update'),
+					'delete' => RoleChecker::isAuth(CustomerController::className(), 'delete'),
+				],
+				'buttons'        => [
+					'view' => function ($url, $model, $key) {
+						return Html::a('<i class="fa fa-shopping-cart" aria-hidden="true"></i>', Url::to([
+							'customer-item/detail',
+							'id' => $model->id,
+						]));
+					},
+				],
+			],
+		],
+	]); ?>
 </div>
