d508adfd7a200749662fcfdf9714de9cb80175f1
loc <loc.xuanphama1t1@gmail.com>
2016-12-31 02:56:34 +0700

    product_history

--- components/widgets/views/topBar.php
+++ components/widgets/views/topBar.php
@@ -323,7 +323,7 @@ use yii\helpers\Url;
 				<!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
 				<li class="dropdown dropdown-user">
 					<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-<!--						<img alt="" class="img-circle" src="http://www.spml.ie/wp-content/uploads/2015/05/no_image_thumb-400x272.gif"/>-->
+						<img alt="" class="img-circle" src="http://www.spml.ie/wp-content/uploads/2015/05/no_image_thumb-400x272.gif"/>
 					<span class="username username-hide-on-mobile">
 					<?=\Yii::$app->getUser()->getIdentity()!=null?\Yii::$app->getUser()->getIdentity()->username:''?> </span>
 						<i class="fa fa-angle-down"></i>
