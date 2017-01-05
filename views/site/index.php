6f2ee734ea28a709771335f848b741bb1e3590b3
kientn16 <ngockien16@gmail.com>
2017-01-05 12:10:05 +0700

    fix create customer

--- views/site/index.php
+++ views/site/index.php
@@ -34,19 +34,19 @@ $this->title = 'Mỹ phẩm Linh Nhâm';
 	<ul class="page-breadcrumb">
 		<li>
 			<i class="fa fa-home"></i>
-			<a href="index.html">Home</a>
+			<a href="<?=\yii\helpers\Url::home()?>">Home</a>
 			<i class="fa fa-angle-right"></i>
 		</li>
 		<li>
 			<a href="#">Trang chủ</a>
 		</li>
 	</ul>
-	<div class="page-toolbar">
-		<div id="dashboard-report-range" class="pull-right tooltips btn btn-sm btn-default" data-container="body" data-placement="bottom" data-original-title="Change dashboard date range">
-			<i class="icon-calendar"></i>&nbsp; <span class="thin uppercase visible-lg-inline-block"></span>&nbsp;
-			<i class="fa fa-angle-down"></i>
-		</div>
-	</div>
+<!--	<div class="page-toolbar">-->
+<!--		<div id="dashboard-report-range" class="pull-right tooltips btn btn-sm btn-default" data-container="body" data-placement="bottom" data-original-title="Change dashboard date range">-->
+<!--			<i class="icon-calendar"></i>&nbsp; <span class="thin uppercase visible-lg-inline-block"></span>&nbsp;-->
+<!--			<i class="fa fa-angle-down"></i>-->
+<!--		</div>-->
+<!--	</div>-->
 </div>
 <?= $this->render('/_alert', [
 	'module' => Yii::$app->getModule('user'),
