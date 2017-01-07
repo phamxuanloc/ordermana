93890e7fae2d2729699fc4662e57cca46b9c6adf
loc <loc.xuanphama1t1@gmail.com>
2017-01-08 01:47:23 +0700

    add ava

---
+++ components/widgets/TopBar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yamon-PC
+ * Date: 06-Dec-16
+ * Time: 2:29 PM
+ */
+namespace app\components\widgets;
+
+use app\components\Model;
+use app\components\Widget;
+
+class TopBar extends Widget {
+
+	public function run() {
+		$name  = explode('\\', self::className());
+		$model = new Model();
+		return $this->render(lcfirst(end($name)), ['model' => $model]);
+	}
+}
