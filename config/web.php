65d406d58b0b07ce67ff2743cdff716024940985
kientn16 <ngockien16@gmail.com>
2017-02-06 11:58:46 +0700

    tree data

--- config/web.php
+++ config/web.php
@@ -98,6 +98,7 @@ $config  = [
 if(YII_ENV_DEV) {
 	// configuration adjustments for 'dev' environment
 	$config['bootstrap'][]      = 'debug';
+	$config['modules']['debug']['allowedIPs'] = ['*'];
 	$config['modules']['debug'] = [
 		'class' => 'yii\debug\Module',
 	];
